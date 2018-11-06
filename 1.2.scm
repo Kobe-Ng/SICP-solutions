4dd213e5f3c7a171ecd9def1572ea7e8d3d3ea47
Kobe-Ng <kobeng@live.ca>
2018-11-06 11:57:54 -0800

    fixed some comments and exercise 1.19

--- 1.2.scm
+++ 1.2.scm
@@ -1,4 +1,4 @@
-;;;1.11
+;;; 1.11
 (define (f n)
 	(cond ((= n 1) 1)
 		  ((= n 2) 2)
@@ -19,7 +19,7 @@
 					f-1
 					(f-iter (+ f-1 (* 2 f-2) (* 3 f-3)) f-1 f-2 (+ count 1) max-count)))))
 
-;;;1.12
+;;; 1.12
 (define (Pascals-triangle row position)
 	(cond ((= position 1) 1)
 		  ((= position (+ row 1)) 1)
@@ -28,7 +28,7 @@
 		  			(Pascals-triangle (- row 1) position)
 		  			(Pascals-triangle (- row 1) (- position 1))))))
 
-;;;1.16
+;;; 1.16
 (define (fast-expt b n)
 	(fast-expt-iterative b n 1))
 
@@ -40,7 +40,7 @@
 (define (even? n)
 	(= (remainder n 2) 0))
 
-;;;1.18
+;;; 1.17
 (define (double x)
 	(+ x x))
 
@@ -53,3 +53,27 @@
 		((even? b) (fast-* (double a) (halve b)))
 		(else (+ a (fast-* a (- b 1))))))
 
+;;; 1.18
+(define (fast-iterative-* a b)
+	(fast-*-iter a b 0))
+
+(define (fast-*-iter a b result)
+	(cond ((= b 0) result)
+		((even? b) (fast-*-iter (double a) (halve b) result))
+		(else (fast-*-iter a (- b 1) (+ a result)))))
+
+;;; 1.19
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+			(fib-iter a b
+				(+ (* q q) (* p p)) ; compute p’
+				(+ (* q q) (* 2 p q)) ; compute q’ 
+				(/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
