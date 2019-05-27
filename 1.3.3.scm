19261e71c50ca72a9a710b9ce43be51631a86a32
Kobe-Ng <kobeng@live.ca>
2019-05-27 12:00:16 +0900

    finished 1.36

--- 1.3.3.scm
+++ 1.3.3.scm
@@ -18,3 +18,25 @@
 				(/ 1 x))) 1)
 
 ;;; 1.36
+(define (fixed-point-p f first-guess)
+	(define (close-enough? v1 v2) 
+		(< (abs (- v1 v2))
+			tolerance)) 
+	(define (try guess)
+		(let ((next (f guess)))
+			(newline)
+			(display guess)
+			(if (close-enough? guess next)
+				next
+          		(try next))))
+  	(try first-guess))
+
+;;; takes 34 steps
+(fixed-point-p (lambda (x) (/ (log 1000) (log x)))
+				2)
+
+(define (average a b)
+	(/ (+ a b) 2))
+
+;;; takes 9 steps with average damping
+(fixed-point-p (lambda (x) (average x (/ (log 1000) (log x)))) 2)
