c7b166443b412ce6c6aff72d09985885124613cf
Kobe-Ng <kobeng@live.ca>
2019-05-30 13:12:30 +0900

    added 2.6

--- chapter 2/2.1.3.scm
+++ chapter 2/2.1.3.scm
@@ -30,3 +30,11 @@
 (define (cdr-2 z)
   (extract-expt 3 z))
 
+;;; 2.6
+
+(define one (lambda (f) (lambda (x) (f x))))
+
+(define two (lambda (f) (lambda (x) (f (f x)))))
+
+(define (add  m n) (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
