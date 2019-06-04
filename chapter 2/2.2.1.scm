558e4dcc162293e640bcb8c50ecadc0f3dd062b1
Kobe-Ng <kobeng@live.ca>
2019-06-04 14:24:00 +0900

    added 2.19

--- chapter 2/2.2.1.scm
+++ chapter 2/2.2.1.scm
@@ -30,3 +30,26 @@
       list1
       (reverse-iter (+ count 1) (cons (list-ref items count) list1))))
   (reverse-iter 0 '()))
+
+;;; 2.19
+
+(define (cc amount coin-values)
+ (cond ((= amount 0) 1)
+       ((or (< amount 0) (no-more? coin-values)) 0) (else
+          (+ (cc amount
+                (except-first-denomination
+                 coin-values))
+            (cc (- amount
+                   (first-denomination
+                    coin-values))
+                coin-values)))))
+
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
