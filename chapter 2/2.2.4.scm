ec50861fde3ec5f20a29c67af2a5cbbc12ac87ef
Kobe-Ng <kobeng@live.ca>
2019-06-18 14:24:11 +0900

    2.46

--- chapter 2/2.2.4.scm
+++ chapter 2/2.2.4.scm
@@ -15,6 +15,25 @@
          (let ((smaller (right-split painter (- n 1))))
               (beside painter (below smaller smaller)))))))
 
+;;; 2.46
 
+(define (make-vect x y)
+  (cons x y))
 
-             
+(define (xcor-vect vect)
+  (car vect))
+
+(define (ycor-vect vect)
+  (cdr vect))
+
+(define (add-vect vect1 vect2)
+  (make-vect (+ (xcor-vect vect1) (xcor-vect vect2))
+             (+ (ycor-vect vect1) (ycor-vect vect2))))  
+
+(define (sub-vect vect1 vect2)
+  (make-vect (- (xcor-vect vect1) (xcor-vect vect2))
+             (- (ycor-vect vect1) (ycor-vect vect2))))
+
+(define (scale-vect c vect)
+  (make-vect (* c (xcor-vect vect))
+             (* c (ycor-vect vect))))  
