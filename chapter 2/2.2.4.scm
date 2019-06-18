5c0d91cfc83c4dabd9b0cc0d977944508f8a64a9
Kobe-Ng <kobeng@live.ca>
2019-06-18 12:28:47 +0900

    added 2.45

--- chapter 2/2.2.4.scm
+++ chapter 2/2.2.4.scm
@@ -5,3 +5,16 @@
      painter
      (let ((smaller (up-split painter (- n 1))))
           (below painter (beside smaller smaller)))))
+
+;;; 2.45
+
+(define (split op1 op2)
+  (lambda (painter n)
+    ((if (= n 0)
+          painter
+         (let ((smaller (right-split painter (- n 1))))
+              (beside painter (below smaller smaller)))))))
+
+
+
+             
