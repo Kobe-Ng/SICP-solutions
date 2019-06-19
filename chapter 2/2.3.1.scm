c17baebd18c598a355593aee43853e25280d58f1
Kobe-Ng <kobeng@live.ca>
2019-06-19 16:18:16 +0900

    2.54

--- chapter 2/2.3.1.scm
+++ chapter 2/2.3.1.scm
@@ -7,3 +7,10 @@
 ;;; #f
 ;;; #f
 ;;; (red shoes blue socks)
+
+;;; 2.54
+
+(define (equal? a b)
+  (cond ((and (null? a) (null? b)) #t)
+        ((not (eq? (car a) (car b))) #f)
+        (else (equal? (cdr a) (cdr b)))))
