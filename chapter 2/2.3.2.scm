232854e9da70bbb7f2113a9b245bac6c6b10f910
Kobe-Ng <kobeng@live.ca>
2019-06-19 17:26:25 +0900

    2.57

--- chapter 2/2.3.2.scm
+++ chapter 2/2.3.2.scm
@@ -69,7 +69,12 @@
         ((and (number? base) (number? power)) (expt base power))
         (else (list '** base power))))
  
- 
- 
- 
+;;; 2.57
+;;; call make-sums on everything 
+(define (augend s)    
+  (accumulate make-sum 0 (cddr s))) 
+  
+(define (multiplicand p)  
+  (accumulate make-product 1 (cddr  p))) 
+
  
