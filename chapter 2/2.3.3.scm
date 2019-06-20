78657ece394da577406c5bc831f6594ff8ce53fa
Kobe-Ng <kobeng@live.ca>
2019-06-20 17:05:01 +0900

    added 2.60

--- chapter 2/2.3.3.scm
+++ chapter 2/2.3.3.scm
@@ -22,3 +22,22 @@
           (union-set (cdr set1) set2))
         (else (cons (car set1) (union-set (cdr set1) set2)))))
 
+;;; 2.60
+
+;;; The functions are quite similar, there is no longer
+;;; any reason to check whether or not an element
+;;; is already in the set.
+;;; element-of-set remains O(n)
+;;; adjoin-set runs in O(1)
+;;; intersection-set runs in O(n^2)
+;;; union-set runs in O(n)
+;;; Sets where we constantly add in repeated elements 
+;;; may become slow in the duplicate representation.
+;;; Intersection and set inspection heavy applications
+;;; become a lot slower.
+;;; In applications where we adjoin and make union
+;;; sets very often the duplicate list has an advantage.
+
+
+
+
