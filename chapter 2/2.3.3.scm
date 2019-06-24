af349f17c7a5ac5b79c89fbb219dfcb01e18d34f
Kobe-Ng <kobeng@live.ca>
2019-06-24 15:29:42 +0900

    2.64 a

--- chapter 2/2.3.3.scm
+++ chapter 2/2.3.3.scm
@@ -69,6 +69,57 @@
 ;;; but I'm not sure how the append will factor in here.
  
 
+ ;;; 2.64
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+ (if (= n 0)
+  (cons '() elts)
+  (let ((left-size (quotient (- n 1) 2)))
+       (let ((left-result
+             (partial-tree elts left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-elts (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+         (let ((this-entry (car non-left-elts))
+               (right-result
+                (partial-tree
+                 (cdr non-left-elts)
+                 right-size)))
+                  (let ((right-tree (car right-result))
+                        (remaining-elts
+                         (cdr right-result)))
+                    (cons (make-tree this-entry
+                                     left-tree
+                                     right-tree)
+                          remaining-elts))))))))
+
+;;; The tree draw from 1 3 5 7 9 11 is 
+
+;;;          5
+;;;         / \
+;;;        1   9
+;;;        \   / \
+;;;        3   7 11
+;;; The algorithm splits the problem into a median entry,
+;;; values smaller than the median, and values larger than the
+;;; median. It then recursively calls itself on the larger
+;;; and smaller values forming smaller trees.
+
+
+
+
+
+
+
 
 
 
