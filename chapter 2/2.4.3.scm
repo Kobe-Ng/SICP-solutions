0d5daf08f65298ef5d12835e7c7cc59f5e97230b
Kobe-Ng <kobeng@live.ca>
2019-07-03 14:08:13 +0900

    2.74c

--- chapter 2/2.4.3.scm
+++ chapter 2/2.4.3.scm
@@ -76,6 +76,21 @@
 (define (get-salary division record) 
   ((get division 'salary) record)) 
   
+;;; c
+(define (find-employee-record employee-name division-list) 
+  (if (null? division-list) 
+       #f 
+       (or (get-record (car division-list) employee-name) 
+           (find-employee-record employee-name (cdr division-list))))) 
+
+
+
+
+
+
+
+
+
 
 
 
