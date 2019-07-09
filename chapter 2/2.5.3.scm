3a076f758f07cfa5ffd48892b8d6bc30fcec379e
Kobe-Ng <kobeng@live.ca>
2019-07-09 16:12:55 +0900

    2.88

--- chapter 2/2.5.3.scm
+++ chapter 2/2.5.3.scm
@@ -5,3 +5,11 @@
 
 (put 'zero? 'polynomial
   (zero? poly))
+
+;;; 2.88
+
+ (define (negate-terms termlist) 
+    (map  (lambda (t) (make-term (order t) (- (coeff t)))) termlist)) 
+
+(define (subtract-poly p1 p2)
+  (add-poly L1 (make-poly (variable p2) (negate-terms (term-list p2)))))
