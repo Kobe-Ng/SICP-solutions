4f73747092197908c1ffacc5856f2e651b061f2c
Kobe-Ng <kobeng@live.ca>
2019-07-11 17:01:02 +0900

    added 3.2

--- chapter 3/3.1.1.scm
+++ chapter 3/3.1.1.scm
@@ -5,3 +5,17 @@
     (lambda (n) 
       (begin (set! initial (+ n initial))
               initial))))
+
+;;; 3.2
+
+(define (make-monitored f)
+  (let ((counter 0))
+    (define (reset-count)
+      (begin (set! counter 0)
+             counter))
+    (define (dispatch m)
+      (cond ((eq? m 'how-many-calls) counter)
+            ((eq? m 'reset-count) (reset-count))
+            (else (begin (set! counter (+ counter 1))
+                         (f m)))))
+dispatch))
