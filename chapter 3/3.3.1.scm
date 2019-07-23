9b0682a0337f805e5b9bf009cd604c4fd1dd656c
Kobe-Ng <kobeng@live.ca>
2019-07-23 16:20:42 +0900

    3.15 isn't great here, but drawing out the block diagram in text is just too annoying

--- chapter 3/3.3.1.scm
+++ chapter 3/3.3.1.scm
@@ -16,3 +16,12 @@
 ;;; It seems like the procedure reverses x
 ;;; The original list is changed into the
 ;;; first element of the list
+
+;;; 3.15
+
+;;; The essence is that set-wow on z1
+;;; changes a to wow, but the second pair
+;;; points to that same a leading to the results
+;;; ((wow b) wow b). For z2, the two elements a
+;;; in the list are distinct, so the result is 
+;;; ((wow b) a b). That second a is not changed.
