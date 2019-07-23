1cf87e5fcd933f12ebcd70caf8f1dc65aaec3d78
Kobe-Ng <kobeng@live.ca>
2019-07-23 15:15:01 +0900

    3.14

--- chapter 3/3.3.1.scm
+++ chapter 3/3.3.1.scm
@@ -10,3 +10,9 @@
 ;;; 3.13
 
 ;;; The interpreter goes into an infinite loop
+
+;;; 3.14
+
+;;; It seems like the procedure reverses x
+;;; The original list is changed into the
+;;; first element of the list
