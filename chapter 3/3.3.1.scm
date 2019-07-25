0411fcea59d7deefa24daeedc1e05af3f21d7d11
Kobe-Ng <kobeng@live.ca>
2019-07-25 15:54:16 +0900

    3.18

--- chapter 3/3.3.1.scm
+++ chapter 3/3.3.1.scm
@@ -49,3 +49,12 @@
 
 ;;; 3.18
 
+ (define (cycle? x) 
+   (define visited nil) 
+   (define (iter x) 
+     (set! visited (cons x visited)) 
+     (cond ((null? (cdr x)) false) 
+           ((memq (cdr x) visited) true) 
+           (else (iter (cdr x))))) 
+   (iter x)) 
+
